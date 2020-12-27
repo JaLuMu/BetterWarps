4d80b3408907fa1510fbb42a9622180a41171734
Jan-Luca Musold <musoldjl@gmail.com>
2020-12-27 15:50:23 +0100

    Update WarpsCommand.java

--- src/main/java/de/jalumu/neowarps/commands/WarpsCommand.java
+++ src/main/java/de/jalumu/neowarps/commands/WarpsCommand.java
@@ -1,6 +1,6 @@
 package de.jalumu.neowarps.commands;
 
-import de.jalumu.neowarps.util.CommandHelper;
+import de.jalumu.neowarps.NeoWarps;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,11 +9,19 @@ import org.bukkit.plugin.java.annotation.command.Commands;
 import org.bukkit.plugin.java.annotation.permission.Permission;
 import org.bukkit.plugin.java.annotation.permission.Permissions;
 
+import static de.jalumu.neowarps.util.CommandHelper.checkPlayer;
+
 @Commands(@org.bukkit.plugin.java.annotation.command.Command(name="warps",desc = "Shows the Warp Menu",permission = "neowarps.warps"))
 @Permissions(@Permission(name = "neowarps.warps",desc = "Provides access to the Warp Menu"))
 public class WarpsCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return true;
+        
+        if (checkPlayer(sender)){
+            NeoWarps.getInstance().getGuiManager().openMainGui((Player) sender);
+            return true;
+        }
+
+        return false;
     }
 }
